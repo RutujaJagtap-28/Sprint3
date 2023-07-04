6928a9efceebc4f76f9c2b979886a0a28f489f02
RutujaJagtap-28 <129721287+RutujaJagtap-28@users.noreply.github.com>
2023-07-04 14:53:17 +0530

    Add files

---
+++ src/main/java/com/rutuja/ItemsApi/service/ItemService.java
@@ -0,0 +1,18 @@
+package com.rutuja.ItemsApi.service;
+
+import com.rutuja.ItemsApi.repository.entity.Item;
+
+import java.util.List;
+
+public interface ItemService
+{
+
+    Item save( Item item );
+
+    void delete( int itemId );
+
+    List<Item> all();
+
+    Item findById( int itemId );
+
+}
